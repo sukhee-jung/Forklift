6fd46fb6749513256081a2541b6fa81b468595b3
sukhee-jung <sukhee.jung@kurlycorp.com>
2024-09-04 09:31:56 +0900

    Create kurly.py

---
+++ kurly.py
@@ -0,0 +1,9 @@
+class KurlyCluster:
+    def __init__(self, channel, name):
+        self.channel = channel
+        self.name = name
+
+# 컬리 클러스터 슬랙 채널 목록
+clusters = [
+    KurlyCluster('C05NXMZP62Y', '김포 클러스터'),
+]
