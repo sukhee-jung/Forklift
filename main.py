af88406f8dc874022e4138dcbf96c87d58a5d8c9
sukhee-jung <sukhee.jung@kurlycorp.com>
2024-12-09 15:16:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main():
         notice_msg = (
             f"안녕하세요? 김포 클러스터 구성원 여러분!\n안전한 사업장 만들기를 위한 안전작업수칙을 안내드립니다.\n\n"
             f"감사합니다.\n"
-            f"*첨부:일: - <https://50072f98-e1d6-4b35-b1e5-5564ad1fcebf.usrfiles.com/ugd/50072f_535ab3cc9a594f068a41b6d49fa98872.xlsx%7C셔틀 노선도>*\n"
+            f"*첨부 : <https://static.wixstatic.com/media/70b014_af73a48c252a4a019d141818de2fe736~mv2.png|안전작업수칙>*\n"
         )
 
         # 메시지 본문
